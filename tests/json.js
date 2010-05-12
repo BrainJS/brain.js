00246581dd6b91077b4ef90a46f149cbf02e7f2b
harthur <fayearthur@gmail.com>
2010-05-11 22:03:05 -0700

    small test runner using asserts

---
+++ tests/json.js
@@ -0,0 +1,22 @@
+var assert = require('assert'),
+    brain = require("../brain");
+
+var net = new brain.NeuralNetwork();
+
+net.train([{input:  {a: Math.random(), b: Math.random()},
+            target: {c: Math.random(), d: Math.random()}},
+           {input:  {a: Math.random(), b: Math.random()},
+            target: {c: Math.random(), d: Math.random()}}]);
+
+var serialized = net.toJSON();
+
+var net2 = new brain.NeuralNetwork({ json: serialized });
+
+var input = {a : Math.random(), b: Math.random()};
+
+var output1 = JSON.stringify(net.run(input));
+var output2 = JSON.stringify(net2.run(input));
+
+assert.ok(output1 == output2);
+
+
---
+++ tests/runner.js
@@ -0,0 +1,14 @@
+var sys = require("sys");
+
+var tests = ["json"];
+
+for(var i = 0; i < tests.length; i++) {
+  var test = tests[i];
+  try {
+    require("./"  + test);
+    sys.puts("PASS " + test);
+  }
+  catch(e) {
+    sys.puts("FAIL " + test + ": " +  e);
+  }
+}
