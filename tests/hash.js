7e309e8f28019e9c52065cf7e9035d846eef6e42
harthur <fayearthur@gmail.com>
2010-05-11 23:24:09 -0700

    add test for sparse hash input/output

---
+++ tests/hash.js
@@ -0,0 +1,38 @@
+var assert = require('assert'),
+    brain = require("../brain");
+
+var net = new brain.NeuralNetwork();
+
+/* sparse hashes for input/output training data */
+net.train([{input:  {a: Math.random(), b: Math.random(), c: Math.random()},
+            target: {A: Math.random(), B: Math.random()}},
+
+           {input:  {d: Math.random(), e: Math.random(), f : Math.random()},
+            target: {C: Math.random()}},
+
+           {input : {}, target : {}}]);
+
+function checkLayer(nodes, layer) {
+  for(var node in nodes)
+    assert.ok(!!layer[nodes[node]], "couldn't find node for " + nodes[node]);
+}
+
+json = net.toJSON();
+var inputNodes = ['a', 'b', 'c', 'd', 'e', 'f'];
+checkLayer(inputNodes, json.layers[0].nodes);
+
+var outputNodes = ['A', 'B', 'C'];
+checkLayer(outputNodes, json.layers[json.layers.length - 1].nodes);
+
+
+/* sparse hash for input running */
+var output = net.run({c: Math.random()});
+checkLayer(outputNodes, output);
+
+/* never seen before input */
+var output = net.run({z: Math.random()});
+checkLayer(outputNodes, output);
+
+/* no input */
+var output = net.run({});
+checkLayer(outputNodes, output);
