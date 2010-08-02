4436f7d89849fee0b8cabf3aee0bcc6e1198c605
Heather Arthur <fayearthur@gmail.com>
2010-08-01 21:14:16 -0700

    separate out neural network code/tests to make room for other modules

--- test/runtests.js
+++ test/runtests.js
@@ -1,13 +1,26 @@
-var sys = require("sys");
+var sys = require("sys"),
+    fs = require("fs"),
+    path = require("path");
 
-var tests = ["json", "hash", "errorthresh", "bitwise", "tofunction", "layers",
-             "grow"];
+function readdirDeepSync(dir) {
+  var files = [];
+  fs.readdirSync(dir).forEach(function(file) {
+    var fname = path.join(dir, file);
+    var stats = fs.statSync(fname);
+    if(stats.isDirectory()) {
+      files = files.concat(readdirDeepSync(fname));
+    } else {
+      files.push(fname);
+    }
+  });
+  return files;
+}
 
-for(var i = 0; i < tests.length; i++) {
-  var test = tests[i];
+function testFile(test) {
+  var test = test.replace(".js", "");
   try {
-    require("./"  + test);
-    sys.puts("PASS " + test);
+    require(test);
+    sys.puts("PASS " + path.basename(test));
   }
   catch(e) {
     var msg = "FAIL " + test + ": " +  e;
@@ -17,3 +30,23 @@ for(var i = 0; i < tests.length; i++) {
     sys.puts(msg);
   }
 }
+
+
+var tests = [];
+if(process.argv.length >= 3) {
+  /* only run target's directory */
+  var target = path.join(__dirname, process.argv[2]);
+  tests = fs.readdirSync(target).map(function(test) {
+    return path.join(target, test);
+  });
+}
+else {
+  /* run all directories of tests */
+  fs.readdirSync(__dirname).forEach(function(file) {
+    var fname = path.join(__dirname, file);
+    if(fs.statSync(fname).isDirectory())
+      tests = tests.concat(tests, readdirDeepSync(fname));
+  });
+}
+
+tests.forEach(testFile);
