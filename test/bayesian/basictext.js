6ebf38c4864081666f49af12973e2e67af347a07
harthur <fayearthur@gmail.com>
2010-09-14 09:13:23 -0700

    localStorage backend for Bayesian classifier

--- test/bayesian/basictext.js
+++ test/bayesian/basictext.js
@@ -1,27 +1,40 @@
 var assert = require('assert'),
     brain = require("../../lib/brain");
 
-var spam = ["vicodin pharmacy",
-            "all quality replica watches marked down",
-            "cheap replica watches",
-            "receive more traffic by gaining a higher ranking in search engines",
-            "viagra pills",
-            "watches chanel tag heuer",
-            "watches at low prices"];
-          
-var not = ["unknown command line parameters",
-           "I don't know if this works on Windows",
-           "recently made changed to terms of service agreement",
-           "does anyone know about this",
-           "this is a bit out of date",
-           "the startup options need linking"]
+function testBasic(bayes) {
+  var spam = ["vicodin pharmacy",
+              "all quality replica watches marked down",
+              "cheap replica watches",
+              "receive more traffic by gaining a higher ranking in search engines",
+              "viagra pills",
+              "watches chanel tag heuer",
+              "watches at low prices"];
 
-var bayes = new brain.BayesianClassifier();
+  var not = ["unknown command line parameters",
+             "I don't know if this works on Windows",
+             "recently made changed to terms of service agreement",
+             "does anyone know about this",
+             "this is a bit out of date",
+             "the startup options need linking"]
 
-spam.forEach(function(text) { bayes.train(text, 'spam'); });
-not.forEach(function(text) { bayes.train(text, 'notspam'); });
+  spam.forEach(function(text) { bayes.train(text, 'spam'); });
+  not.forEach(function(text) { bayes.train(text, 'notspam'); });
 
-assert.equal(bayes.classify("replica watches"),"spam");
-assert.equal(bayes.classify("check out the docs"), "notspam");
-assert.equal(bayes.classify("recently, I've been thinking that I should"), "notspam");
-assert.equal(bayes.classify("come buy these cheap pills"), "spam");
+  assert.equal(bayes.classify("replica watches"),"spam");
+  assert.equal(bayes.classify("check out the docs"), "notspam");
+  assert.equal(bayes.classify("recently, I've been thinking that I should"), "notspam");
+  assert.equal(bayes.classify("come buy these cheap pills"), "spam");
+}
+
+// test the synchronous backends
+//testBasic(new brain.BayesianClassifier());
+
+testBasic(new brain.BayesianClassifier({
+  backend : {
+    type: 'localStorage',
+    options: {
+      name: 'testnamespace',
+      testing: true 
+    } 
+  }
+}));
