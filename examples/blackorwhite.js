6c6ea2bba6327c0c74abda6cf384c97774fba714
Heather Arthur <fayearthur@gmail.com>
2011-11-06 21:44:44 -0800

    explain more things

--- examples/blackorwhite.js
+++ examples/blackorwhite.js
@@ -9,7 +9,7 @@ var utils = {
   randomColor : function() {
     return { r: Math.round(Math.random() * 255), 
              g: Math.round(Math.random() * 255),
-             b: Math.round(Math.random() * 255)};
+             b: Math.round(Math.random() * 255) };
   },
 
   toRgb : function(color) {
