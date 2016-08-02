83102f023e766fc821d441a64b91d5fb48905ea7
Robert Plummer <robertleeplummerjr@gmail.com>
2016-08-01 20:13:48 -0400

    working rnn added utility methods for going to and from a vocab or indexes

--- test/unit/recurrent/math.js
+++ test/unit/recurrent/math.js
@@ -1,81 +1,33 @@
 var fs = require('fs');
+var assert = require('assert');
 var RNN = require('../../../lib/recurrent/rnn');
-var vocabData = initVocab();
-
-function initVocab() {
-  var d = ['0','1','2','3','4','5','6','7','8','9','+','='];
-
-  // filter by count threshold and create pointers
-  var characterToIndex = {};
-  var indexToCharacter = {};
-  var vocab = [];
-  // NOTE: start at one because we will have START and END tokens!
-  // that is, START token will be index 0 in model letter vectors
-  // and END token will be index 0 in the next character softmax
-  for(var i = 0; i < d.length; i++) {
-    var ch = d[i];
-    // add character to vocab
-    characterToIndex[ch] = i;
-    indexToCharacter[i] = ch;
-    vocab.push(ch);
-  }
-
-  return {
-    characterToIndex: characterToIndex,
-    indexToCharacter: indexToCharacter,
-    distinct: vocab.join(''),
-    inputSize: vocab.length + 1,
-    outputSize: vocab.length + 1
-  };
-}
-
-function phraseToIndexes(phrase, maxThreshold) {
-  maxThreshold = maxThreshold || 0;
-  var result = [];
-  var characterToIndex = vocabData.characterToIndex;
-
-  for (var i = 0, max = phrase.length; i < max; i++) {
-    var character = phrase[i];
-    var index = characterToIndex[character];
-    if (index < maxThreshold) continue;
-    result.push(index);
-  }
-  
-  return result;
-}
-
-function indexesToPhrase(indexes, maxThreshold) {
-  maxThreshold = maxThreshold || 0;
-  var result = [];
-  var indexToCharacter = vocabData.indexToCharacter;
-
-  for (var i = 0, max = indexes.length; i < max; i++) {
-    var index = indexes[i];
-    if (index < maxThreshold) continue;
-    var character = indexToCharacter[index];
-    result.push(character);
-  }
-
-  return result;
-}
+var Vocab = require('../../../lib/recurrent/vocab');
+var vocab = new Vocab(['0','1','2','3','4','5','6','7','8','9','+','=', '-', '/', '*']);
 
 function randomMath() {
-  return Math.floor(Math.random() * 10) + '+' + Math.floor(Math.random() * 10);
+  var left = Math.floor(Math.random() * 10);
+  var right = Math.floor(Math.random() * 10);
+  return left + '+' + right + '=' + (left + right);
 }
 
-describe('character', function() {
-  it('', function() {
-    var rnn = new RNN({
-      inputSize: 11,
-      outputSize: 11
+describe('rnn', function() {
+  describe('math', function() {
+    it('can predict what a math problem is after being fed 1000 random math problems', function() {
+      var rnn = new RNN({
+        inputSize: vocab.characters.length + 1,
+        outputSize: vocab.characters.length + 1,
+        vocab: vocab
+      });
+
+      for (var i = 0; i < 1000; i++) {
+        rnn.inputVocab(randomMath());
+      }
+
+      for (i = 0; i < 5; i++) {
+        var prediction = rnn.predictVocab();
+        assert(/[+]/.test(prediction));
+        assert(/[=]/.test(prediction));
+      }
     });
-
-    for (var i = 0; i < 1000; i++) {
-      rnn.input(phraseToIndexes(randomMath()));
-    }
-
-    var prediction = rnn.predict();
-
-    console.log(indexesToPhrase(prediction).join(''));
   });
 });
---
+++ test/unit/recurrent/vocab.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var Vocab = require('../../../lib/recurrent/vocab');
+
+describe('vocab', function() {
+  describe('#toIndexes', function() {
+    it('should properly be able to reference indexes of cat', function() {
+      var vocab = new Vocab(['cat']);
+      var asIndexes = [1, 2, 3];
+      vocab.toIndexes('cat').forEach(function(v, i) {
+        assert(v === asIndexes[i]);
+      });
+    });
+    it('should properly be able to reference indexes of math', function() {
+      var vocab = new Vocab(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', '+']);
+      var asIndexes = [1, 12, 9, 11, 9];
+      vocab.toIndexes('0+8=8').forEach(function(v, i) {
+        assert(v === asIndexes[i]);
+      });
+    });
+  });
+  describe('#toCharacters', function() {
+    it('should properly be able to reference characters of cat', function() {
+      var vocab = new Vocab(['cat']);
+      var asIndexes = [1, 2, 3];
+      var asCharacters = 'cat';
+      vocab.toCharacters(asIndexes).forEach(function(v, i) {
+        assert(v === asCharacters[i]);
+      });
+    });
+  });
+});
