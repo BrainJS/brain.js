b3bfe8495b7b79c6ebedc48c2f0ad5bd7f954396
Robert Plummer <robertleeplummerjr@gmail.com>
2016-08-01 21:25:00 -0400

    test lstm

--- test/unit/recurrent/math.js
+++ test/unit/recurrent/math.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var assert = require('assert');
 var RNN = require('../../../lib/recurrent/rnn');
+var LSTM = require('../../../lib/recurrent/lstm');
 var Vocab = require('../../../lib/recurrent/vocab');
 var vocab = new Vocab(['0','1','2','3','4','5','6','7','8','9','+','=', '-', '/', '*']);
 
@@ -10,8 +11,8 @@ function randomMath() {
   return left + '+' + right + '=' + (left + right);
 }
 
-describe('rnn', function() {
-  describe('math', function() {
+describe('math', function() {
+  describe('rnn', function() {
     it('can predict what a math problem is after being fed 1000 random math problems', function() {
       var rnn = new RNN({
         inputSize: vocab.characters.length + 1,
@@ -25,6 +26,25 @@ describe('rnn', function() {
 
       for (i = 0; i < 5; i++) {
         var prediction = rnn.predictVocab();
+        assert(/[+]/.test(prediction));
+        assert(/[=]/.test(prediction));
+      }
+    });
+  });
+  describe('lstm', function() {
+    it('can predict what a math problem is after being fed 1000 random math problems', function() {
+      var lstm = new LSTM({
+        inputSize: vocab.characters.length + 1,
+        outputSize: vocab.characters.length + 1,
+        vocab: vocab
+      });
+
+      for (var i = 0; i < 1000; i++) {
+        lstm.inputVocab(randomMath());
+      }
+
+      for (i = 0; i < 5; i++) {
+        var prediction = lstm.predictVocab();
         assert(/[+]/.test(prediction));
         assert(/[=]/.test(prediction));
       }
