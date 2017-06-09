1b269acdaa99d3d861d339c93372f0ba1e764b34
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-06-09 18:52:24 +0530

    array check and tests

---
+++ test/utilities/mse.js
@@ -0,0 +1,23 @@
+import assert from 'assert';
+ import toArray from '../../src/utilities/to-array';
+ import zeros from '../../src/utilities/zeros';
+ 
+ describe('toArray', () => {
+     it('should return the same array if an array are passed', () => {
+         let collection = zeros(10);
+         let temp = toArray(collection);
+         assert.ok(collection.prototype === temp.prototype);
+     });
+ 
+     it('should return an array if object is passed', () => {
+         let collection = {
+             name: 'Steve Jobs',
+             alive: false
+         }
+ 
+         let temp = toArray(collection);
+ 
+         assert.ok(Array.isArray(temp));
+         assert.ok(temp.length === Object.keys(collection).length);
+     })
+ })
