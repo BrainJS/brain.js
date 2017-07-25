dfffb88f802eba6b8572d9099af96e054e413d43
gabrielfreire <gabriel.freire@imsmaxims.com>
2017-07-25 15:15:32 +0100

    Small fix

--- src/recurrent/matrix/sigmoid.js
+++ src/recurrent/matrix/sigmoid.js
@@ -5,7 +5,7 @@
 export default function sigmoid(product, left) {
   // sigmoid nonlinearity
   for(let i=0; i < left.weights.length; i++) {
-    product.weights[i] = sig(-left.weights[i]);
+    product.weights[i] = sig(left.weights[i]);
     product.deltas[i] = 0;
   }
 }
