df5d6e3cde8d08250c749699b6d2a89d3e38b7cc
Robert Plummer <robertleeplummerjr@gmail.com>
2018-01-26 15:00:47 -0500

    move toFunction into its own test, for easier debugging add more tests, exposing issue resolve issue which was expecting `layer` as an index, where `i` is actual index, and we need to set `this.biases[i][j]` which is a float, not overwrite it

---
+++ test/base/to-function.js
@@ -0,0 +1,19 @@
+import assert from 'assert';
+import NeuralNetwork from '../../src/neural-network';
+
+describe('.toFunction()', () => {
+  const originalNet = new NeuralNetwork();
+  const xorTrainingData = [
+    {input: [0, 0], output: [0]},
+    {input: [0, 1], output: [1]},
+    {input: [1, 0], output: [1]},
+    {input: [1, 1], output: [0]}];
+  originalNet.train(xorTrainingData);
+  const xor = originalNet.toFunction();
+  it.only('runs same as original network', () => {
+    assert.deepEqual(xor([0, 0])[0].toFixed(8), originalNet.run([0, 0])[0].toFixed(8));
+    assert.deepEqual(xor([0, 0])[0].toFixed(8), originalNet.run([0, 1])[0].toFixed(8));
+    assert.deepEqual(xor([0, 0])[0].toFixed(8), originalNet.run([1, 0])[0].toFixed(8));
+    assert.deepEqual(xor([0, 0])[0].toFixed(8), originalNet.run([1, 1])[0].toFixed(8));
+  });
+});
