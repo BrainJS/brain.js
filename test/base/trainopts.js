2f1655956d900ffc2a43d32b2b69466b7742989e
Fred Christensen <fred@ifit.com>
2018-02-16 11:25:22 -0700

    # If applied, this commit will make life better for everyone
    # Why is this change needed?
    To help with peace on earth
    
    # How does it address the issue?
    takes test from 90 seconds to 4 seconds

--- test/base/trainopts.js
+++ test/base/trainopts.js
@@ -74,102 +74,78 @@ describe('train() options', () => {
   });
 });
 
+describe('train() and trainAsync() use the same internal methods', () => {
+  
+  it(' _prepTraining()', (done) => {
+    let trainingData = [ { input: [0, 0], output: [0] } ];
+    var net = new brain.NeuralNetwork();
+    let tick = 0;
+    net._prepTraining = (d, o) => {
+      tick++;
+      if (tick === 2) return done ();
+      return net._prepTraining(d, o);
+    };
+    net.train(trainingData, {iterations: 1});
+    assert.ok(false);
+    net.trainAsync(trainingData, {iterations: 1});
+  });
 
-describe('async train() options', () => {
-  it('train until error threshold reached', (done) => {
-    let net = new brain.NeuralNetwork();
-    let p1 = net
-      .trainAsync(data, { errorThresh: 0.2 })
-      .then (res => {
-        assert.ok(res.error < 0.2, `[res.error, ${res.error}] should have been less then 0.2`);
-        done();
-      })
-      .catch(err => { assert.ok(false, err.toString()) });
-  }).timeout(10000);
+  it(' _updateTrainingOptions()', (done) => {
+    let trainingData = [ { input: [0, 0], output: [0] } ];
+    var net = new brain.NeuralNetwork();
+    let tick = 0;
+    net._updateTrainingOptions = (o) => {
+      tick++;
+      if (tick === 2) return done ();
+      return net._updateTrainingOptions(o);
+    };
+    net.train(trainingData, {iterations: 1});
+    assert.ok(false);
+    net.trainAsync(trainingData, {iterations: 1});
+  });
 
-  it('train until max iterations reached', (done) => {
-    let net = new brain.NeuralNetwork();
-    let res = net
-      .trainAsync(data, { iterations: 25 })
-      .then(res => {
-        assert.equal(res.iterations, 25, `[res.iterations, ${res.iterations}] should have been less then 25`);
-        done();
-      })
-      .catch(err => { assert.ok(false, err.toString()) });
-  }).timeout(10000);
+  it(' _formatData()', (done) => {
+    let trainingData = [ { input: [0, 0], output: [0] } ];
+    var net = new brain.NeuralNetwork();
+    let tick = 0;
+    net._formatData = (d) => {
+      tick++;
+      if (tick === 2) return done ();
+      return net._formatData(d);
+    };
+    net.train(trainingData, {iterations: 1});
+    assert.ok(false);
+    net
+      .trainAsync(trainingData, {iterations: 1})
+      .then()
+      .catch(assert.ifError);
+  });
 
-  it('asyinc training callback called with training stats', (done) => {
-    let iters = 100;
-    let period = 20;
-    let target = iters / period;
+  it(' _verifyIsInitialized()', (done) => {
+    let trainingData = [ { input: [0, 0], output: [0] } ];
+    var net = new brain.NeuralNetwork();
+    let tick = 0;
+    net._verifyIsInitialized = (d) => {
+      tick++;
+      if (tick === 2) return done ();
+      return net._verifyIsInitialized(d);
+    };
+    net.train(trainingData, {iterations: 1});
+    assert.ok(false);
+    net.trainAsync(trainingData, {iterations: 1});
+  });
 
-    let calls = 0;
-
-    let net = new brain.NeuralNetwork();
-    net.trainAsync(data, {
-      iterations: iters,
-      callbackPeriod: period,
-      callback: (res) => {
-        assert.ok(res.iterations % period == 0);
-        calls++;
-      }
-    })
-    .then (res => {
-      assert.ok(target === calls, `[calls, ${calls}] should be the same as [target, ${target}]`);
-      done();
-    })
-    .catch(err => { assert.ok(false, err.toString()) });
-  }).timeout(10000);
-
-  it('learningRate ASYNC - higher learning rate should train faster', (done) => {
-    let data = [
-      { input: [0, 0], output: [0] },
-      { input: [0, 1], output: [1] },
-      { input: [1, 0], output: [1] },
-      { input: [1, 1], output: [1] }
-    ];
-
-    let net = new brain.NeuralNetwork();
-    let net2 = new brain.NeuralNetwork();
-
-    let p1 = net.trainAsync(data, { learningRate: 0.5 });
-    let p2 = net2.trainAsync(data, { learningRate: 0.8 });
-
-    Promise
-      .all([p1, p2])
-      .then(values => {
-        let res = values[0];
-        let res2 = values[1];
-        assert.ok(res.iterations > (res2.iterations * 1.1), `${res.iterations} !> ${res2.iterations * 1.1}`);
-        done();
-      })
-      .catch(err => {
-        assert.ok(false, err.toString())
-      });
-  }).timeout(10000);
-
-  it('momentum ASYNC - higher momentum should train faster', (done) => {
-    let data = [
-      { input: [0, 0], output: [0] },
-      { input: [0, 1], output: [1] },
-      { input: [1, 0], output: [1] },
-      { input: [1, 1], output: [1] }
-    ];
-
-    let net = new brain.NeuralNetwork({ momentum: 0.1 });
-    let net2 = new brain.NeuralNetwork({ momentum: 0.5 });
-
-    let p1 = net.trainAsync(data);
-    let p2 = net2.trainAsync(data);
-
-    Promise.all([p1, p2])
-      .then(values => {
-        let res = values[0];
-        let res2 = values[1];
-        assert.ok(res.iterations > (res2.iterations * 1.1), `${res.iterations} !> ${res2.iterations * 1.1}`);
-        done();
-      }).catch(err => {
-        assert.ok(false, err.toString())
-      });
-  }).timeout(10000);
+  it(' _trainingTick()', (done) => {
+    let trainingData = [ { input: [0, 0], output: [0] } ];
+    var net = new brain.NeuralNetwork();
+    let tick = 0;
+    net._trainingTick = (d, o, e) => {
+      tick++;
+      if (tick === 2) return done ();
+      return net._trainingTick(d, o, e);
+    };
+    net.train(trainingData, {iterations: 1});
+    assert.ok(false);
+    net.trainAsync(trainingData, {iterations: 1});
+  });
 });
