5fdec97fa583dc7828b6d1c136160b7bf46abf4d
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-06-27 18:05:50 +0500

    remove dist for node.js,build scripts (+ minified), update cdn link

--- README.md
+++ README.md
@@ -134,7 +134,7 @@ bower install brain.js
 
 #### Use CDN
 
-[brain.js CDN Link](unpkg.com/brain.js): Use this link to directly include `brain.js` script in your webpage.
+[brain.js CDN Link](unpkg.com/brain.js/dist/brain.min.js): Use this link to directly include `brain.js` script in your webpage.
 
 _This is recommended way to load brain.js in browser._
 
@@ -142,7 +142,7 @@ Or
 
 #### Download from Github
 
-[Download brain.js for browser](https://cdn.rawgit.com/BrainJS/brain.js/master/browser.js)
+[Download brain.js for browser](https://cdn.rawgit.com/BrainJS/brain.js/master/dist/brain.min.js)
 
 **Note**: Training is computationally expensive, so you should try to train the network offline (or on a Worker) and use the `toFunction()` or `toJSON()` options to plug the pre-trained network into your website.
 
--- src/index.js
+++ src/index.js
@@ -11,19 +11,45 @@ import RNNTimeStep from './recurrent/rnn-time-step';
 import LSTMTimeStep from './recurrent/lstm-time-step';
 import GRUTimeStep from './recurrent/gru-time-step';
 
-export default {
-  crossValidate,
-  likely,
-  lookup,
-  NeuralNetwork,
-  NeuralNetworkGPU,
-  TrainStream,
-  recurrent: {
-    RNN,
-    LSTM,
-    GRU,
-    RNNTimeStep,
-    LSTMTimeStep,
-    GRUTimeStep
-  }
+var utilities = {
+  max: require('./utilities/max').default,
+  mse: require('./utilities/mse').default,
+  ones: require('./utilities/ones').default,
+  random: require('./utilities/random').default,
+  randomWeight: require('./utilities/random-weight').default,
+  randos: require('./utilities/randos').default,
+  range: require('./utilities/range').default,
+  toArray: require('./utilities/to-array').default,
+  DataFormatter: require('./utilities/data-formatter').default,
+  zeros: require('./utilities/zeros').default,
 };
+
+var brain = {
+  crossValidate: crossValidate,
+  likely: likely,
+  lookup: lookup,
+  NeuralNetwork: NeuralNetwork,
+  NeuralNetworkGPU: NeuralNetworkGPU,
+  TrainStream: TrainStream,
+  recurrent: {
+    RNNTimeStep: RNNTimeStep,
+    LSTMTimeStep: LSTMTimeStep,
+    GRUTimeStep: GRUTimeStep,
+    RNN: RNN,
+    LSTM: LSTM,
+    GRU: GRU,
+  },
+  utilities: utilities,
+};
+
+if (typeof window !== 'undefined') {
+  window.brain = brain;
+}
+
+if (typeof self !== 'undefined') {
+  self.brain = brain;
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = brain;
+}
