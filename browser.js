3e8c6caad884e539b1161e5c61a4667d10992906
Robert Plummer <robertleeplummerjr@gmail.com>
2018-11-04 15:29:23 -0500

    fix: Include index.js in build (oops!)

--- browser.js
+++ browser.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: Heather Arthur <fayearthur@gmail.com>
  *   homepage: https://github.com/brainjs/brain.js#readme
- *   version: 1.4.4
+ *   version: 1.4.7
  *
  * acorn:
  *   license: MIT (http://opensource.org/licenses/MIT)
