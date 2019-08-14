1ac059935d467e05c63d84e0d22b7d01822f0188
Robert Plummer <robertleeplummerjr@gmail.com>
2019-08-14 17:11:51 -0400

    fix: Add sigmoid coverage

---
+++ __tests__/activation/sigmoid.js
@@ -0,0 +1,14 @@
+const sigmoid = require('../../src/activation/sigmoid');
+
+describe('sigmoid', () => {
+  describe('.active()', () => {
+    it('matches for value 1', () => {
+      expect(sigmoid.activate(1).toFixed(5)).toBe('0.73106');
+    });
+  });
+  describe('.measure()', () => {
+    it('matches for value .7', () => {
+      expect(sigmoid.measure(0.7, .5).toFixed(5)).toBe('0.10500');
+    });
+  });
+});
