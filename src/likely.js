818eb2dc9f2cd0f43ac8387870c2a511a467447f
Muhammad Ubaid Raza <mubaidr@gmail.com>
2020-05-01 13:55:10 +0500

    imrpove error message in likely

--- src/likely.js
+++ src/likely.js
@@ -5,11 +5,17 @@
  * @returns {*}
  */
 module.exports = function likely(input, net) {
+  if (!net) {
+    throw new TypeError(
+      `Required parameter 'net' is of type ${typeof net}. Must be of type 'brain.NeuralNetwork'`
+    );
+  }
+
   const output = net.run(input);
   let maxProp = null;
   let maxValue = -1;
 
-  Object.keys(output).forEach(key => {
+  Object.keys(output).forEach((key) => {
     const value = output[key];
     if (value > maxValue) {
       maxProp = key;
