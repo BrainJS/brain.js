0f2e18cc9cbcc46581b24b64a0ee9cf72ee2e76d
vorticalbox <vorticalbox@protonmail.com>
2020-05-20 14:40:38 +0100

    update cast to typescript

--- src/utilities/cast.ts
+++ src/utilities/cast.ts
@@ -1,21 +1,23 @@
-function arraysToFloat32Arrays(arrays) {
+// TODO: update any to types
+
+function arraysToFloat32Arrays(arrays: any[]) {
   const result = [];
   for (let i = 0; i < arrays.length; i++) {
     result.push(Float32Array.from(arrays[i]));
   }
   return result;
 }
-function arrayToFloat32Arrays(array) {
+function arrayToFloat32Arrays(array: any[]) {
   const result = [];
   for (let i = 0; i < array.length; i++) {
     result.push(Float32Array.from([array[i]]));
   }
   return result;
 }
-function arrayToFloat32Array(array) {
+function arrayToFloat32Array(array: number[]) {
   return Float32Array.from(array)
 }
-function objectsToFloat32Arrays(objects, table, length) {
+function objectsToFloat32Arrays(objects: { [k: string]: any }, table: { [k: string]: any }, length: number) {
   const results = [];
   for (let i = 0; i < objects.length; i++) {
     const object = objects[i];
@@ -29,14 +31,14 @@ function objectsToFloat32Arrays(objects, table, length) {
   }
   return results;
 }
-function objectToFloat32Arrays(object) {
+function objectToFloat32Arrays(object: { [x: string]: number; }) {
   const result = [];
   for (let p in object) {
     result.push(Float32Array.from([object[p]]));
   }
   return result;
 }
-function objectToFloat32Array(object, table, length) {
+function objectToFloat32Array(object: { [k: string]: any }, table: { [k: string]: any }, length: number) {
   const result = new Float32Array(length);
   for (let p in object) {
     if (object.hasOwnProperty(p)) {
@@ -46,11 +48,11 @@ function objectToFloat32Array(object, table, length) {
   return result;
 }
 
-module.exports = {
+export default {
   arraysToFloat32Arrays,
   arrayToFloat32Arrays,
   arrayToFloat32Array,
   objectsToFloat32Arrays,
   objectToFloat32Arrays,
   objectToFloat32Array,
-};
+}
