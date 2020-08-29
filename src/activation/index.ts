05873f3df6e3aa29074267940bb73149c00d6e30
HarshKhandeparkar <mjkgoa@gmail.com>
2020-08-29 09:55:42 +0530

    fix: import -> export

--- src/activation/index.ts
+++ src/activation/index.ts
@@ -1,4 +1,4 @@
-import * as relu from './relu';
-import * as sigmoid from './sigmoid';
-import * as tanh from './tanh';
+export * as relu from './relu';
+export * as sigmoid from './sigmoid';
+export * as tanh from './tanh';
 export * as leakyRelu from './leaky-relu';
