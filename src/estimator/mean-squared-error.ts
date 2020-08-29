e1e12df6d2a7fb00d97b2ccd558e9cbd6055cc41
HarshKhandeparkar <mjkgoa@gmail.com>
2020-08-29 16:00:07 +0530

    refactor<estimator>: convert mean-squared-error to ts

--- src/estimator/mean-squared-error.ts
+++ src/estimator/mean-squared-error.ts
@@ -1,7 +1,15 @@
-const { makeKernel } = require('../utilities/kernel');
+import { makeKernel } from '../utilities/kernel';
+import { IKernelRunShortcut } from 'gpu.js';
 
-function mse2d(errors) {
-  // mean squared error 2d
+/**
+ * 2D Mean Squared Error
+ */
+export function mse2d(
+  this: {
+    constants: {height: number, width: number, length: number}
+  },
+  errors: [number, number][]
+) {
   let sum = 0;
   for (let y = 0; y < this.constants.height; y++) {
     for (let x = 0; x < this.constants.width; x++) {
@@ -11,8 +19,13 @@ function mse2d(errors) {
   return sum / this.constants.length;
 }
 
-class MeanSquaredError {
-  constructor({ width, height }) {
+export class MeanSquaredError {
+  calculate: IKernelRunShortcut;
+  addAbsolute: IKernelRunShortcut;
+  add: IKernelRunShortcut;
+  divide: IKernelRunShortcut;
+
+  constructor({width, height}: {width: number, height: number}) {
     this.calculate = makeKernel(mse2d, {
       output: [1],
       constants: {
@@ -22,8 +35,10 @@ class MeanSquaredError {
       },
       immutable: true,
     });
+
+    // Need more info about what value1 and value2 is.
     this.addAbsolute = makeKernel(
-      function (value1, value2) {
+      function (value1: any, value2: any) {
         return value1[0] + Math.abs(value2[0][0]);
       },
       {
@@ -31,8 +46,9 @@ class MeanSquaredError {
         immutable: true,
       }
     );
+
     this.add = makeKernel(
-      function (value1, value2) {
+      function (value1: number[], value2: number[]) {
         return value1[0] + value2[0];
       },
       {
@@ -40,8 +56,9 @@ class MeanSquaredError {
         immutable: true,
       }
     );
+
     this.divide = makeKernel(
-      function (length, mseSum) {
+      function (length: number, mseSum: number[]) {
         const value = mseSum[0];
         if (value > 0) {
           return value / length;
@@ -55,5 +72,3 @@ class MeanSquaredError {
     );
   }
 }
-
-module.exports = { MeanSquaredError };
