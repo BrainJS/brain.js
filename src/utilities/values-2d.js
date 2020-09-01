416d6050e3d3af5fabc8d7bb1029746603232288
HarshKhandeparkar <mjkgoa@gmail.com>
2020-09-02 01:16:09 +0530

    refactor: convert utilities/values* to typescript

--- src/utilities/values-2d.js
+++
@@ -1,9 +0,0 @@
-const values = require('./values');
-
-module.exports = function values2D(width, height, value) {
-  const result = new Array(height);
-  for (let y = 0; y < height; y++) {
-    result[y] = values(width, value);
-  }
-  return result;
-};
--- src/utilities/values-3d.js
+++
@@ -1,9 +0,0 @@
-const values2D = require('./values-2d');
-
-module.exports = function values3D(width, height, depth, value) {
-  const result = new Array(depth);
-  for (let z = 0; z < depth; z++) {
-    result[z] = values2D(width, height, value);
-  }
-  return result;
-};
