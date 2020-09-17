54e33cc7ca49ee42268ed407beca816863cc4fa6
ofirgeller <ofirgeller22@gmail.com>
2020-09-17 18:12:46 +0300

    turn array-lookup-table.js to typescript

---
+++ src/utilities/array-lookup-table.ts
@@ -0,0 +1,19 @@
+export class ArrayLookupTable<TDatum extends { [key: string]: any[] }> {
+
+  length: number = 0;
+  table: any = {};
+
+  constructor(data: TDatum[], public prop: keyof TDatum) {
+    const table = (this.table = {});
+    for (let i = 0; i < data.length; i++) {
+      const datum = data[i];
+      const input = datum[prop];
+      for (let j = 0; j < input.length; j++) {
+        for (const p in input[j]) {
+          if (this.table.hasOwnProperty(p)) continue;
+          this.table[p] = this.length++;
+        }
+      }
+    }
+  }
+}
