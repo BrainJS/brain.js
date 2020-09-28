a916961a04536b30daf3eb9618a5bc085f59df9c
Robert Plummer <robertleeplummerjr@gmail.com>
2020-09-28 15:14:39 -0400

    feat: Migrate rnn-cell.js to rnn-cell.ts and tests

--- __tests__/layer/rnn-cell.ts
+++ __tests__/layer/rnn-cell.ts
@@ -1,17 +1,13 @@
-const { rnnCell } = require('../../src/layer/rnn-cell');
+import { rnnCell } from '../../src/layer/rnn-cell';
+import { mockLayer } from '../test-utils';
+import { RecurrentZeros } from '../../src/layer/recurrent-zeros';
 
 describe('Recurrent Layer', () => {
   test('properly sets width and height', () => {
-    const input = { width: 1, height: 3 };
+    const input = mockLayer({ width: 1, height: 3 });
 
     const settings = { height: 3 };
-    const recurrentInput = {
-      setDimensions: (width, height) => {
-        recurrentInput.width = width;
-        recurrentInput.height = height;
-      },
-    };
-
+    const recurrentInput = new RecurrentZeros();
     const layer = rnnCell(settings, input, recurrentInput);
 
     expect(layer.width).toEqual(1);
--- src/layer/rnn-cell.ts
+++ src/layer/rnn-cell.ts
@@ -1,13 +1,23 @@
+import { ILayer, ILayerSettings } from './base-layer';
+import { RecurrentZeros } from './recurrent-zeros';
+
 const { relu } = require('./relu');
 const { add } = require('./add');
 const { multiply } = require('./multiply');
 const { random } = require('./random');
 const { zeros } = require('./zeros');
 
-function rnnCell(settings, input, recurrentInput) {
+export function rnnCell(
+  settings: ILayerSettings,
+  input: ILayer,
+  recurrentInput: RecurrentZeros
+): ILayer {
   const { height } = settings;
 
-  if (recurrentInput.setDimensions) recurrentInput.setDimensions(1, height);
+  if (typeof height !== 'number') throw new Error('height not set');
+  if (recurrentInput.setDimensions) {
+    recurrentInput.setDimensions(1, height);
+  }
 
   // wxh
   const weight = random({
@@ -33,7 +43,3 @@ function rnnCell(settings, input, recurrentInput) {
     )
   );
 }
-
-module.exports = {
-  rnnCell,
-};
