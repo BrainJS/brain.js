08f6aa26cc83262e747c31542f48edc700b1d9d6
Yash Shah <yashah1234@gmail.com>
2020-10-04 16:24:23 +0530

    minor: fixed a single type in range.ts

--- src/utilities/range.ts
+++ src/utilities/range.ts
@@ -5,7 +5,7 @@
  * @returns {Array}
  */
 export function range(start: number, end: number): number[] {
-  const result = [];
+  const result: number[] = [];
   for (; start < end; start++) {
     result.push(start);
   }
