558b12fcb15c3ad39bffd50bec6e65459766df55
Croustys <akos.barabas2002@gmail.com>
2020-10-07 19:31:12 +0200

    flatten-layers converted to ts

--- src/utilities/flatten-layers.ts
+++ src/utilities/flatten-layers.ts
@@ -1,15 +1,16 @@
+import { ILayer } from '../layer/base-layer';
 const traverseLayersFrom = require('./traverse-layers-from');
 
-module.exports = function flattenLayers(layers) {
+export default function flattenLayers(layers: ILayer[]) {
   const result = layers.slice(0);
   for (let i = 0; i < result.length; i++) {
     let offset = 0;
-    traverseLayersFrom(result[i], (layer) => {
-      if (result.indexOf(layer) === -1) {
+    traverseLayersFrom(result[i], (layer: ILayer) => {
+      if (!result.includes(layer)) {
         result.splice(i + offset, 0, layer);
         offset++;
       }
     });
   }
   return result;
-};
+}
