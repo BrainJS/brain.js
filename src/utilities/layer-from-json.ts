831dab6532a0bd81c881b9b7ce21ff897feb73a8
Michał Antropik <michal.antropik@gmail.com>
2020-10-17 19:15:25 +0200

    remove lodash from layer-from-json

--- src/utilities/layer-from-json.ts
+++ src/utilities/layer-from-json.ts
@@ -1,18 +1,24 @@
 import * as layer from '../layer';
-import { get } from 'lodash';
 import { ILayer, ILayerJSON } from '../layer/base-layer';
+
+const getStringKeyValue = (object: any, path: string | string[], defaultValue: any) => {
+  const pathArray = Array.isArray(path) ? path : path.split('.').filter(key => key)
+  const pathArrayFlat = pathArray.flatMap(part => typeof part === 'string' ? part.split('.') : part)
+
+  return pathArrayFlat.reduce((obj, key) => obj && obj[key], object) || defaultValue
+}
 
 export default function layerFromJSON(jsonLayer: ILayerJSON, inputLayer1?: ILayer) { 
   if (!layer.hasOwnProperty(jsonLayer.type)) return null;
 
-  const Layer: Function = get(layer, `${layer}.${jsonLayer.type}`);
+  const Layer: Function = getStringKeyValue(layer, `${layer}.${jsonLayer.type}`, inputLayer1);
 
   // eslint-disable-next-line
   const realLayer: ILayer | any = Reflect.construct(Layer, arguments);
 
   Object.keys(jsonLayer).forEach((param: string) => {
     if (param !== 'type') {
-      realLayer[param] = get(jsonLayer, `${jsonLayer}.${param}`);
+      realLayer[param] = getStringKeyValue(jsonLayer, `${jsonLayer}.${param}`, inputLayer1);
     }
   });
 
