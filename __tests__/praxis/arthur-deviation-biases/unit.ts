5d1a6dfd316a6ab1cc38eb23784ba31011d82405
Arihant Jain <jainanilkumar739@gmail.com>
2020-10-22 13:41:06 +0530

    Minor fix in file __tests__/praxis/arthur-deviation-biases/unit.ts

--- __tests__/praxis/arthur-deviation-biases/unit.ts
+++ __tests__/praxis/arthur-deviation-biases/unit.ts
@@ -33,7 +33,7 @@ describe('ArthurDeviationBiases Class: Unit', () => {
           momentum: 0.2,
         },
       });
-      const result: any = kernel(weights, deltas);
+      const result: Float32Array = kernel(weights, deltas);
       const value = new Float32Array([1.25, 2.20000005, 3.1500001]);
       expect(shave(result)).toEqual(shave(value));
     });
