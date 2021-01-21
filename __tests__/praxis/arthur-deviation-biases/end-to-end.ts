3dd2641d9d44683bd2ba0fe23965d8e138d96f7c
Robert Plummer <robertleeplummerjr@gmail.com>
2021-01-21 06:07:54 -0500

    fix: Unit test

--- __tests__/praxis/arthur-deviation-biases/end-to-end.ts
+++ __tests__/praxis/arthur-deviation-biases/end-to-end.ts
@@ -35,37 +35,36 @@ describe('ArthurDeviationBiases', () => {
       net.train(xorTrainingData, {
         iterations: 1,
       });
-      // name is not exists in IRandomSettings
-      const layer1 = random({ id: 'biases', height: 3 } as any);
+      const layer1 = random({ id: 'biases', height: 3 });
       const praxis = new ArthurDeviationBiases(layer1, {
-        learningRate: (net.trainOpts as any).learningRate,
+        learningRate: net.trainOpts.learningRate,
       });
-      expect((praxis as any).learningRate).toBe(
-        (net.trainOpts as any).learningRate
+      expect(praxis.settings.learningRate).toBe(
+        net.trainOpts.learningRate
       );
 
       net.deltas[0][0] = 1;
       net.deltas[0][1] = 2;
 
-      (layer1.deltas as any)[0][0] = (net.deltas as any)[1][0] = 3;
-      (layer1.deltas as any)[1][0] = (net.deltas as any)[1][1] = 4;
-      (layer1.deltas as any)[2][0] = (net.deltas as any)[1][2] = 5;
+      (layer1.deltas as number[][])[0][0] = (net.deltas as any)[1][0] = 3;
+      (layer1.deltas as number[][])[1][0] = (net.deltas as any)[1][1] = 4;
+      (layer1.deltas as number[][])[2][0] = (net.deltas as any)[1][2] = 5;
 
       (net.deltas as any)[2][0] = 6;
 
-      (layer1.weights as any)[0][0] = (net.biases as any)[1][0] = 7;
-      (layer1.weights as any)[1][0] = (net.biases as any)[1][1] = 8;
-      (layer1.weights as any)[2][0] = (net.biases as any)[1][2] = 9;
+      (layer1.weights as number[][])[0][0] = (net.biases as any)[1][0] = 7;
+      (layer1.weights as number[][])[1][0] = (net.biases as any)[1][1] = 8;
+      (layer1.weights as number[][])[2][0] = (net.biases as any)[1][2] = 9;
       (net.biases as any)[2][0] = 10;
       net.adjustWeights();
       praxis.setupKernels();
-      const result = praxis.run(layer1);
-      expect((result as any)[0][0]).not.toBe(0);
-      expect((result as any)[0][0]).toBe((net.biases as any)[1][0]);
-      expect((result as any)[1][0]).not.toBe(0);
-      expect((result as any)[1][0]).toBe((net.biases as any)[1][1]);
-      expect((result as any)[2][0]).not.toBe(0);
-      expect((result as any)[2][0]).toBe((net.biases as any)[1][2]);
+      const result = praxis.run(layer1) as number[][];
+      expect((result)[0][0]).not.toBe(0);
+      expect((result)[0][0]).toBe((net.biases as any)[1][0]);
+      expect((result)[1][0]).not.toBe(0);
+      expect((result)[1][0]).toBe((net.biases as any)[1][1]);
+      expect((result)[2][0]).not.toBe(0);
+      expect((result)[2][0]).toBe((net.biases as any)[1][2]);
     });
   });
 });
