6d87491b5fe34990c75108a59522e17fdbd345b5
Bar Admoni <badmoni@cisco.com>
2021-04-30 20:21:15 +0300

    fix: Fix unit tests
    Fix a few unit tests that always were failling

--- src/layer/modifier.ts
+++ src/layer/modifier.ts
@@ -19,17 +19,20 @@ export class Modifier extends BaseLayer {
 
   validate(): void {
     super.validate();
+
     if (this.width !== this.inputLayer.width) {
       throw new Error(
         `width of ${this.width} does not match inputLayer.width of ${this.inputLayer.width}`
       );
     }
+
     if (this.height !== this.inputLayer.height) {
       throw new Error(
         `height of ${this.height} does not match inputLayer.height of ${this.inputLayer.height}`
       );
     }
-    if (this.depth !== this.inputLayer.depth) {
+
+    if (this.depth !== (this.inputLayer.depth ?? 0)) {
       throw new Error(
         `depth of ${this.depth} does not match inputLayer.depth of ${this.inputLayer.depth}`
       );
